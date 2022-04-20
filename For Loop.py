7ec892389c7ed783bc6843daf24a1a38402f4e24
Mrxalfin <79769231+AlfinHidayatullah@users.noreply.github.com>
2022-04-20 15:08:12 +0700

    Add files via upload
    Tutorial Looping

---
+++ For Loop.py
@@ -0,0 +1,9 @@
+#contoh pengulangan for sederhana
+angka = [1,2,3,4,5]
+for x in angka:
+    print(x)
+
+#contoh pengulangan for
+    buah = ["nanas", "apel", "jeruk"]
+    for makanan in buah:
+        print ("Saya suka makan", makanan)
